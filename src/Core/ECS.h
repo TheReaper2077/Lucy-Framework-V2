ce756c74036a65a277ca04e4a096054dcce72f40
TheSuperKDK <sreenikalarikkal@gmail.com>
2022-05-14 21:05:24 +0530

    test

---
+++ src/Core/ECS.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <entt/entt.hpp>
+#include <unordered_map>
+
+namespace lf {
+	class registry: public entt::registry {
+		std::unordered_map<std::size_t, entt::entity> singleton_instances;
+	
+	public:
+		template <typename T, typename ...Args>
+		T& store(Args ...params) {
+			if (singleton_instances.find(typeid(T).hash_code()) == singleton_instances.end()) {
+				singleton_instances[typeid(T).hash_code()] = this->create();
+				this->emplace<T>(singleton_instances[typeid(T).hash_code()], params...);
+			}
+
+			return this->get<T>(singleton_instances[typeid(T).hash_code()]);
+		}
+	};
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "Core/ECS.h"
+#include <iostream>
+
+int main(int ArgCount, char **Args) {
+	std::cout << "dskfkd\n";
+	entt::registry reg;
+}
